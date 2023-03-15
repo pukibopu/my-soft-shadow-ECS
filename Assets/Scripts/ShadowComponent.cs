d95abbc3d82c01ed8234fd5e1e9fdceeba9a4875
jinjo <creationer625@outlook.com>
2023-03-15 19:10:11 +0800

    separate data

--- Assets/Scripts/ShadowComponent.cs
+++ Assets/Scripts/ShadowComponent.cs
@@ -19,24 +19,5 @@ public class ShadowComponent:IComponentData
     public float LightWidth;
 }
 
-public class ShadowMapECS : MonoBehaviour, IConvertGameObjectToEntity
-{
-    public ShadowResolution shadowResolution = ShadowResolution.Medium;
-    public float bias = 0.0001f;
-    public float shadowStrength = 1.0f;
-    public float shadowFilterStride = 1.0f;
-    public float lightWidth = 0.5f;
-     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
-     {
-         var tempDate = new ShadowComponent
-         {
-            Resolution = shadowResolution,
-            Bias = bias,
-            ShadowStrength = shadowStrength,
-            ShadowFilterStride = shadowFilterStride,
-            LightWidth = lightWidth
-         };
-         dstManager.AddComponentData(entity, tempDate);
-     }
-}
+
 
